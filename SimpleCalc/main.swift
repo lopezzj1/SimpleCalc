8d13943f3ae0575af7b48910f0514eced5347619
lopezzj1 <lopezzj1@uw.edu>
2015-10-06 19:50:15 -0700

    Initial Commit

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Jill Lopez on 10/6/15.
+//  Copyright © 2015 Jill Lopez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
