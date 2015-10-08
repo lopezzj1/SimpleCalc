400e53ebfefeec64707205c2e7fcc5e57c9295fe
lopezzj1 <lopezzj1@uw.edu>
2015-10-08 14:04:46 -0700

    code for a command line application with swift creating a calculator

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -8,5 +8,81 @@
 
 import Foundation
 
-print("Hello, World!")
+func input() -> String {
+    let keyboard = NSFileHandle.fileHandleWithStandardInput()
+    let inputData = keyboard.availableData
+    let result = NSString(data: inputData, encoding:NSUTF8StringEncoding) as! String
+    return result.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+}
+
+print("Welcome to SimplyCalc")
+
+print("Enter any expression separated by returns:")
+
+
+var count = 0   //counter
+
+var total = 0   //running total
+
+var arrayNums = [String]()
+
+var firstVar = String(input())  //first input
+
+var secondVar = String(0)   //second input
+
+arrayNums.append(firstVar)
+
+count++
+
+//get next input
+var nextInput = String(input())
+
+var isStillTyping = true
+
+switch nextInput {
+    case "+", "-", "*", "/":
+        if nextInput == "+" {
+            secondVar = String(input())
+            total = Int(firstVar)! + Int(secondVar)!
+        } else if nextInput == "-" {
+            secondVar = String(input())
+            total = Int(firstVar)! - Int(secondVar)!
+        } else if nextInput == "*" {
+            secondVar = String(input())
+            total = Int(firstVar)! * Int(secondVar)!
+        } else if nextInput == "/" {
+            secondVar = String(input())
+            total = Int(firstVar)! / Int(secondVar)!
+        }
+        print("Result: \(total)")
+    
+    case "fact":
+        total = Int(firstVar)!
+        for var index = Int(firstVar)! - 1; index > 0; index-- {
+            total = total * index
+        }
+        print("Result: \(total)")
+    default:
+        while isStillTyping{
+            if nextInput == "avg" {
+                for value in arrayNums {
+                    total += Int(value)!
+                }
+                total = total/count
+                print("Results: \(total)")
+                break
+            }
+            if nextInput == "count" {
+                print("Results: \(count)")
+                break
+            }
+            
+            arrayNums.append(nextInput)
+            count++
+            nextInput = input()
+            
+        }
+        break
+}
+
 
